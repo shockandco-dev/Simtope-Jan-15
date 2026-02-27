6dd2536fe2417333bd0ce53a322a857e9553f6a5
shockandco <shockandco@gmail.com>
2026-02-27 16:44:33 +0000

    fix: Exclude dev code from production build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: './',
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          // This will remove all console.log statements from the production build.
+          drop_console: true,
+        },
+      },
+    },
+    // This will prevent the WebSocket code from being included in the production build.
+    define: {
+      'process.env.NODE_ENV': `"${mode}"`,
+    },
+  }
 })
